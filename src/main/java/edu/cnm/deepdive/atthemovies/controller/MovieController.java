8fbb435d1075875158f139475e1cdfe3a1f0c624
Anita <amartin98@cnm.edu.com>
2019-07-02 09:13:14 -0600

    Start Actor portion MVC.

--- src/main/java/edu/cnm/deepdive/atthemovies/controller/MovieController.java
+++ src/main/java/edu/cnm/deepdive/atthemovies/controller/MovieController.java
@@ -3,14 +3,19 @@ package edu.cnm.deepdive.atthemovies.controller;
 import edu.cnm.deepdive.atthemovies.model.dao.MovieRepository;
 import edu.cnm.deepdive.atthemovies.model.entity.Movie;
 import edu.cnm.deepdive.atthemovies.model.entity.Movie.Genre;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.ExposesResourceFor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("movies")
+@ExposesResourceFor(Movie.class)
 public class MovieController {
 
     private MovieRepository repository;
@@ -21,18 +26,26 @@ public class MovieController {
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    List<Movie> list(@RequestParam(value = "genre", required = false) Genre genre) {
+    public List<Movie> list(@RequestParam(value = "genre", required = false) Genre genre) {
         if (genre == null) {
             return repository.getAllByOrderByTitleAsc();
         } else {
             return repository.getAllByGenreOrderByTitleAsc(genre);
         }
-
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    Movie post(@RequestBody Movie movie){
-        return repository.save(movie); //TODO Build a ResponseEntity.
+    public Movie post(@RequestBody Movie movie) {
+        return repository.save(movie); // TODO Build a ResponseEntity.
     }
+
+    @GetMapping(value = "{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public Movie get(@PathVariable("id") UUID id) {
+        return repository.findById(id).get();
+    }
+
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoSuchElementException.class)
+    public void notFound() {}
 }
---
+++ src/main/java/edu/cnm/deepdive/atthemovies/model/entity/Actor.java
@@ -0,0 +1,5 @@
+package edu.cnm.deepdive.atthemovies.model.entity;
+
+//TODO Add annotations, fields, and methods to make this an Entity class.
+public class Actor {
+}
