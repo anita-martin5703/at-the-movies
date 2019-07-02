293f3be78669947663dbb10ed9670521eccca68e
Anita <amartin98@cnm.edu.com>
2019-07-02 09:39:38 -0600

    Fixed Errors have SQL

--- src/main/java/edu/cnm/deepdive/atthemovies/model/entity/Actor.java
+++ src/main/java/edu/cnm/deepdive/atthemovies/model/entity/Actor.java
@@ -1,5 +1,30 @@
 package edu.cnm.deepdive.atthemovies.model.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.UUID;
+
 //TODO Add annotations, fields, and methods to make this an Entity class.
+@Entity
 public class Actor {
+    @Id
+    private UUID id;
+
+    private String name;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
